acef8391bc6ab06dd3ba25839212930dc355b7f3
ps-shoaib <shoaib@purgesol.com>
2022-06-17 13:12:08 +0500

    testing issues

--- IGHPortalAPI/Startup.cs
+++ IGHPortalAPI/Startup.cs
@@ -137,6 +137,8 @@ namespace IGHportalAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             //if (env.IsDevelopment())
             //{
             //    app.UseDeveloperExceptionPage();
